307e440a2aae9ac44b54230de550bb5384f271b3
Loxeryz <loxerya@gmail.com>
2025-12-21 17:37:14 +0700

    Create bgd.lua

---
+++ Library/bgd.lua
@@ -0,0 +1,12 @@
+local BackgroundTheme = {
+    'https://i.pinimg.com/736x/4a/9a/7c/4a9a7ce30a03b5276594ab45c12a30d1.jpg',
+    'https://i.pinimg.com/736x/ab/48/92/ab489205310a76c95ed8a9a3c2c73956.jpg',
+    'https://i.pinimg.com/736x/45/be/80/45be8031c2de03ce2be45501d1e56f26.jpg',
+    'https://i.pinimg.com/736x/eb/e6/36/ebe636803f2e034c7bec62ff52893059.jpg',
+    'https://i.pinimg.com/736x/83/32/83/83328386b2c52c37c2a857c26b3bb04f.jpg',
+    'https://i.pinimg.com/736x/c0/dc/39/c0dc39a283c1a0485181441e860fa716.jpg',
+    'https://i.pinimg.com/736x/86/f4/f8/86f4f8566083cc29e07c863641a3fe1b.jpg',
+    'https://i.pinimg.com/736x/cd/e0/40/cde040a05052c0670350ce945c39fdcb.jpg'
+}
+
+return BackgroundTheme
