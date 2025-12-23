8f9970acf7e95745762217b7bf3746d11d7b2974
Loxeryz <loxerya@gmail.com>
2025-12-23 21:04:56 +0700

    Create afqRemote.lua

---
+++ plugins/afqRemote.lua
@@ -0,0 +1,10 @@
+local allRemoteCode = {
+    "\030", -- play
+    "\v", -- redeem
+    "\018", -- gear spin
+    '\025', -- weapon spin
+    '\026', -- skill
+    "\f", -- hit attack
+    '\022' -- craft
+}
+return allRemoteCode
